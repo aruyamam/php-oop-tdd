8194c5586cbab4b4e2d6ca0bc43e04b4f8eed7d2
aruyamam <aruyamam@gmail.com>
2019-07-27 13:32:05 +0900

    Repository Pattern

---
+++ bug-report-app/src/contracts/RepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Entity\Entity;
+
+interface RepositoryInterface
+{
+   public function find(int $id): ?object;
+   public function findOneBy(string $field, $value): ?object;
+   public function findBy(string $criteria);
+   public function findAll(int $id): array;
+   public function sql(string $query);
+   public function create(Entity $entity): object;
+   public function update(Entity $entity, array $conditions): object;
+   public function delete(Entity $entity, array $conditions): void;
+}
